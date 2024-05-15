4d7d4e8be40743e1e7cd4630db514a6439d5316d
nicolas-valognes <nicolas@run-motion.com>
2024-05-15 16:54:53 +0200

    add maths and test

--- test/utils.js
+++ test/utils.js
@@ -1,4 +1,3 @@
-
 import assert from "assert";
 
 import * as utils from"../utils/utils.js";
--- utils/utils.js
+++ utils/utils.js
@@ -2,6 +2,3 @@ export function greeting(name = 'you')  {
     return `Hello ${name}! How are you?`;
 }
 
-export function getMarathonDistanceinMeters() {
-    return 42192;
-}
